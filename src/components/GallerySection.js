cd9dcbe50e5df870cf1d2220677e0ef6c94d7b04
Panescu Cosmin <1panescu.cosmin@gmail.com>
2023-04-21 22:38:21 +0300

    Finishgallery, skills and testimonial sections

--- src/components/GallerySection.js
+++ src/components/GallerySection.js
@@ -1,7 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+// gallery data
+import { galleryData } from '../data';
+// photo album
+import { PhotoAlbum } from 'react-photo-album';
+// framer motion
+import { motion } from 'framer-motion';
+// animation
+import { fadeIn } from '../variants';
+
+// slides
+const slides = galleryData.images.map(({ original, width, height }) => ({
+  src: original,
+  width,
+  height,
+}));
 
 const GallerySection = () => {
-  return <div>GallerySection</div>;
+  // index state
+  const [index, setIndex] = useState(-1);
+  // destructure gallery data
+  const { title, btnText, btnIcon, images } = galleryData;
+
+  return (
+    <section className="relative bg-[#f9f9f9] section mt-[40px]">
+      {/* title */}
+      <motion.div
+        className="container mx-auto"
+        variants={fadeIn('up')}
+        initial='hidden'
+        whileInView={'show'}
+        viewport={{ once: false, amount: 0.6 }}
+      >
+        <h2 className='h2 max-w-[370px]'>{title}</h2>
+      </motion.div>
+      {/* gallery */}
+      <motion.div
+        className='mb-8 lg:mb-20'
+        variants={fadeIn('up')}
+        initial='hidden'
+        whileInView={'show'}
+        viewport={{ once: false, amount: 0.2 }}
+      >
+        <PhotoAlbum
+          photos={images}
+          layout='rows'
+          onClick={(event, photo, index) => setIndex(index)}
+        />
+      </motion.div>
+      {/* btn */}
+      <motion.div
+        className='flex justify-center'
+        variants={fadeIn('up')}
+        initial='hidden'
+        whileInView={'show'}
+        viewport={{ once: false, amount: 0.2 }}
+      >
+        <button className='btn btn-lg btn-dark'>
+          {btnText}
+          <div className='text-xl'>{btnIcon}</div>
+        </button>
+      </motion.div>
+    </section>
+  )
 };
 
 export default GallerySection;
--- src/components/Interview.js
+++ src/components/Interview.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+//  interview data
+
+import { interviewData } from '../data';
+// framer motion
+import { motion } from 'framer-motion';
+// modal video
+import ModalVideo from 'react-modal-video';
+import '../modalVideo.scss';
+// animation
+import { fadeIn } from '../variants';
 
 const Interview = () => {
+  // destructure interview data
+  const { title, btnText, btnIcon } = interviewData;
+  // open video state
+  const [isOpen, setIsOpen] = useState(false);
+
   return <div>Interview</div>;
 };
 
